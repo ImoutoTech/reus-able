7b927e7aa1c7850baec7cb950b032929cc40e816
季悠然 <youranreus@qq.com>
2025-02-16 16:47:54 +0800

    feat: 项目结构调整

--- packages/nestjs/common/decorator/AuthRole.ts
+++ packages/nestjs/common/decorator/AuthRole.ts
@@ -2,3 +2,5 @@ import { SetMetadata } from '@nestjs/common';
 import { AuthRoleType } from '@reus-able/types';
 
 export const AuthRoles = (...roles: AuthRoleType[]) => SetMetadata('roles', roles);
+
+export const PermissionGuard = (...permissions: string[]) => SetMetadata('permissions', permissions);
--- packages/nestjs/index.ts
+++ packages/nestjs/index.ts
@@ -1,7 +1,6 @@
 export * from './common/exceptions';
 export * from './common/guard';
 export * from './common/interceptors';
-export * from './common/logger';
 export * from './common/decorator';
 export * from './common/middleware';
 export * from './common/module';
